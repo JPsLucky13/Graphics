d37d0c2771cdc2c6b95bc8d4e4abcff7c0b401c0
JPsLucky13 <jeanpaulpeschard43@gmail.com>
2018-01-27 00:55:42 -0700

    Successfully added translation

--- Peschard_JeanPaul_ICG_CS6610/main.cpp
+++ Peschard_JeanPaul_ICG_CS6610/main.cpp
@@ -1,8 +1,13 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <../../Utilities/cyTriMesh.h>
+#include <../../Utilities/cyGL.h>
+#include <../../Utilities/cyMatrix.h>
 
 //Clear color values
+const int screenWidth = 512;
+const int screenHeight = 512;
+
 float red = 0.25f;
 float green = 0.1f;
 float blue = 0.2f;
@@ -12,6 +17,28 @@ float colorChangeSpeed = 0.005f;
 GLuint VBO;
 GLuint VAO;
 cy::TriMesh mesh;
+cy::GLSLProgram shaderProgram;
+
+//Mouse states
+bool leftMouseDown = false;
+bool rightMouseDown = false;
+
+//Mouse Positions
+cy::Point3f currentMousePosition;
+cy::Point3f lastMousePosition;
+cy::Point3f mouseDirection;
+
+//Mouse Rotations
+cy::Point3f currentMouseYRotation;
+cy::Point3f lastMouseYRotation;
+cy::Point3f mouseYRotationDirection;
+
+//Matrices
+cy::Matrix4<float> cameraPositionMatrix = cy::Matrix4<float>::MatrixTrans(cy::Point3f(0.0f,0.0f,-50.0f));
+cy::Matrix4<float> cameraRotationMatrix = cy::Matrix4<float>::MatrixRotationX(0.0f);
+cy::Matrix4<float> viewMatrix;
+cy::Matrix4<float> projectionMatrix = cy::Matrix4<float>::MatrixPerspective(0.785398f, screenWidth / screenHeight, 0.1f, 100);
+cy::Matrix4<float> mvp;
 
 void CreateMesh()
 {
@@ -23,6 +50,11 @@ void Display()
 	//glClearColor(GLclampf(red), GLclampf(green), GLclampf(blue), GLclampf(alpha));
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	viewMatrix = cameraPositionMatrix * cameraRotationMatrix;
+	mvp = projectionMatrix * viewMatrix;
+	shaderProgram.Bind();
+	shaderProgram.SetUniformMatrix4("mvp",mvp.data);
+	glDrawArrays(GL_POINTS, 0, mesh.NV());
 	glutSwapBuffers();
 }
 
@@ -35,6 +67,65 @@ void Keyboard(unsigned char key, int x, int y)
 			glutLeaveMainLoop();
 	}
 }
+
+void Mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		lastMouseYRotation = cy::Point3f(0.0f, static_cast<float>(x), 0.0f);
+		leftMouseDown = true;
+	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+	{
+		leftMouseDown = false;
+	}
+
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		lastMousePosition = cy::Point3f(0.0f, 0.0f, static_cast<float>(y));
+		rightMouseDown = true;
+	}
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
+	{
+		rightMouseDown = false;
+	}
+
+
+
+}
+
+void MouseMovement(int x, int y)
+{
+	//Update the mouse
+
+	//Camera Rotation
+	if (leftMouseDown)
+	{
+		currentMouseYRotation = cy::Point3f(0.0f, static_cast<float>(x), 0.0f);
+
+		mouseYRotationDirection = currentMouseYRotation - lastMouseYRotation;
+
+		lastMouseYRotation = cy::Point3f(0.0f, static_cast<float>(x), 0.0f);
+
+
+		cy::Matrix4<float> oldMouseYRotation = cameraRotationMatrix;
+		cameraRotationMatrix.SetRotationY(mouseYRotationDirection.y);
+
+	}
+
+	//Camera Translation
+	if (rightMouseDown)
+	{
+		currentMousePosition = cy::Point3f(0.0f,0.0f, static_cast<float>(y));
+
+		mouseDirection = currentMousePosition - lastMousePosition;
+		
+		lastMousePosition = cy::Point3f(0.0f, 0.0f, static_cast<float>(y));
+
+		cameraPositionMatrix.AddTrans(mouseDirection);
+	}
+}
+
 
 void Idle()
 {
@@ -56,23 +147,33 @@ void Idle()
 	{
 		blue = 0.0f;
 	}
-
-
 	glutPostRedisplay();
 }
 
+void CreateShaders()
+{
+	//The Shader Program
+	cy::GLSLShader vertexShader;
+	vertexShader.CompileFile("Shaders/Vertex/mesh.cgfx",GL_VERTEX_SHADER);
+	cy::GLSLShader fragmentShader;
+	fragmentShader.CompileFile("Shaders/Fragment/mesh.cgfx", GL_FRAGMENT_SHADER);
+	shaderProgram.Build(&vertexShader,&fragmentShader);
+	
+}
 
-
-int main(int argc, char* argv[])
+int main(int argc, char* argv)
 {
 	//Initialize GLUT
-	glutInit(&argc, argv);
+	glutInit(&argc, &argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(512, 512);
+	glutInitWindowSize(screenWidth, screenHeight);
 	glutInitWindowPosition(0,0);
-	glutCreateWindow("Hello World");
+	glutCreateWindow("Hello Teapot!");
 	glutKeyboardFunc(Keyboard);
-	
+	glutMouseFunc(Mouse);
+	glutMotionFunc(MouseMovement);
+	glutDisplayFunc(Display);
+	glutIdleFunc(Idle);
 	
 	//Initialize GLEW
 	glewInit();
@@ -84,10 +185,11 @@ int main(int argc, char* argv[])
 	glBufferData(GL_ARRAY_BUFFER, mesh.NV() * sizeof(cy::Point3f), &mesh.V(0), GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS,0,1);
+	
+	
+	
+	CreateShaders();
 
-	glutDisplayFunc(Display);
-	glutIdleFunc(Idle);
 	glutMainLoop();
 
 	return 0;
